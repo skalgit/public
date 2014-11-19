c9d96102995034739cf0e8fa3ecbbb30581bb78b
email <abaster@abaster.buy4now.ie>
2014-11-19 13:18:45 +0000

    commit address and enums

---
+++ Enums/AddressType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum AddressType
+    {
+        Billing = 1,
+        ShippingPrimary = 2,
+        ShippingSecondary = 3
+    }
+}
